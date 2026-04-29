ab142f0c53ebee07071f97ebc7afb7a2d8ce7279
resolvectl <116422609+resolvectl@users.noreply.github.com>
2026-04-29 20:15:13 +0300

    ru checks + fzf + code improvements

--- iperf.sh
+++ iperf.sh
@@ -1,81 +1,120 @@
 #!/usr/bin/env bash
 
-set -uo pipefail
+set -euo pipefail
 
-FILENAME=$(basename "$0")
+declare regions=("Russia" "Europe" "Asia" "North America" "South America" "Oceania" "Africa")
+declare menu_options=("Start iperf3 test" "Fetch newest iperf lists" "Quit")
 
-function usage()
-{
-    cat << EOU >&2
-$FILENAME Usage:
 
-$FILENAME --help            Display help
-$FILENAME --fetch           Fetch newest iperf3 servers 
-EOU
+function detect_os {
+    if [[ -f /etc/debian-release ]]; then
+        echo "debian"
+    elif [[ -f /etc/redhat-release || -f /etc/centos-release ]]; then
+        echo "rhel"
+    else
+        echo "unknown"
+    fi
+
 }
 
 function check_requirements {
-    for cmd in jq fping iperf3 curl; do
-    if ! command -v "$cmd" &> /dev/null; then
-        echo "Failed to start. '$cmd' required to executte script." >&2
-        exit 1
-    fi
-done
-}
+        local required_packages=("fzf" "jq" "fping" "iperf3" "curl")
+        local missing_packages=()
 
-function run_iperf_test {
-    cmd=$(cat iperf/iperf_servers.json | jq -r '.[] | select(.CONTINENT=="Europe")."IP/HOST"+"|"+."PORT"+"|"+."SITE"+"|"+."COUNTRY"+"|"+."PROVIDER"')
-    servers=$(echo "$cmd" | cut -d'|' -f1)
-    total=$(echo "$servers" | wc -l)
-    echo "$total servers saved. Running tests ..." 
+        os_type=$(detect_os)
 
-    best_ip=$(fping -e -q -C 1 -r 0 -B 1 -4 -t 300 "$servers" 2>&1 \
-        | awk '$3 > 0 {print $1, int($3)}' \
-        | sort -k2 -n \
-        | head -1 )
-    formatted_ip=$(awk '{print $1}' <<< "$best_ip")
-    ping=$(awk '{print $2}' <<< "$best_ip")
-    best_server=$(grep "^${formatted_ip}|" <<< "$cmd")    
-    IFS='|' read -r host port city country provider <<< "$best_server"
-
-    echo "Best server: $host $ping ms $provider ($city, $country)"
-    echo "Starting iperf test ..."
-    iperf3 -c "$host" -p "$port" -P1
+        for cmd in "${required_packages[@]}"; do
+            if ! command -v "$cmd" &> /dev/null; then
+                missing_packages+=("$cmd")
+            fi
+        done
+        if ((${#missing_packages[@]})); then
+            case "$os_type" in
+            debian)
+                sudo apt install "${missing_packages[@]}" -y && menu || exit 1
+                ;;
+            rhel)
+                sudo dnf install -y "${missing_packages[@]}" && menu || exit 1
+                ;;
+            *)
+                echo "Failed to install: Unknown OS. Install requirements manually" || exit 1
+                ;;            
+            esac  
+        fi
 }
 
 function fetch_iperf {
-    echo "Obtaining newest iperf3 servers ..."
-    curl https://export.iperf3serverlist.net/listed_iperf3_servers.json > iperf/new_iperf_servers.json \
-    && mv iperf/new_iperf_servers.json iperf/iperf_servers.json \
-    || echo "Failed to fetch newest iperf servers"
-
-}
-
-function execute {
-    if [[ ! -d iperf ]]; then
-        mkdir "iperf"
-    fi
-    if [[ -f iperf/iperf_servers.json ]]; then
-        run_iperf_test
+    mkdir -p iperf
+    if curl https://export.iperf3serverlist.net/listed_iperf3_servers.json -o iperf/new_iperf_servers.json; then
+        mv iperf/new_iperf_servers.json iperf/iperf_servers.json
     else
-        fetch_iperf && execute
+        echo "Failed to reach https://export.iperf3serverlist.net/listed_iperf3_servers.json"
+        read -n 1 -s -r -p "Press any key to continue ..."
+    fi
+    if curl https://raw.githubusercontent.com/itdoginfo/russian-iperf3-servers/refs/heads/main/list.yml -o iperf/new_ru_iperf.yml; then
+        mv iperf/new_ru_iperf.yml iperf/ru_iperf.yaml
+    else
+        echo "Failed to reach https://raw.githubusercontent.com/itdoginfo/russian-iperf3-servers/refs/heads/main/list.yml"
+        read -n 1 -s -r -p "Press any key to continue ..."
+    fi
+    menu
+}
+
+function run_iperf_test  {
+    cmd=$1
+    servers=$(echo "$cmd" | cut -d'|' -f1)
+    total=$(echo "$servers" | wc -l)
+    echo "$total servers fetched. testing ..." 
+    best_ip=$(
+        (echo "$servers" | xargs -r fping -e -q -C 1 -r 0 -B 1 -4 -t 300 2>&1 || true) \
+        | awk '$3 > 0 {print $1, int($3)}' \
+        | sort -k2 -n \
+        | head -1)
+    formatted_ip=$(awk '{print $1}' <<< "$best_ip")
+    ping=$(awk '{print $2}' <<< "$best_ip")
+    best_server=$(grep "^${formatted_ip}|" <<< "$cmd")    
+    IFS='|' read -r host port city country isp <<< "$best_server"
+
+    echo "Best server: $host $ping ms $isp ($city, $country)"
+    echo "Starting iperf test ..."
+    if iperf3 -c "$host" -p "$port" -P1; then
+        read -n 1 -s -r -p "Press any key to continue ..."
+        menu
+    else 
+        echo "Failed to start iperf3 test"
+        read -n 1 -s -r -p "Press any key to continue ..."
+        menu
     fi
 }
 
-while true
-do
-	case "${@}" in
-		-h | --help)
-			usage
-			exit 0
-			;;
-        --fetch)
+function choose_region {
+    choose=$(printf "%s\n" "${regions[@]}" | fzf --reverse ) || menu
+    if [[ $choose == "Russia" ]]; then
+        cmd=$(yq '.[] | "\(.address)|\(.port)|\(.City)|RU|\(.Name)"' iperf/ru_iperf.yaml) \
+        && run_iperf_test "$cmd" \
+        || read -n 1 -s -r -p "Fetch servers first. Press any key to continue ..."; menu
+
+    else
+        cmd=$(cat iperf/iperf_servers.json | jq -r --arg choose "$choose" '.[] | select(.CONTINENT == $choose)."IP/HOST"+"|"+."PORT"+"|"+."SITE"+"|"+."COUNTRY"+"|"+."PROVIDER"')
+        run_iperf_test "$cmd"
+    fi
+}
+
+function menu {
+    choose=$(printf "%s\n" "${menu_options[@]}" | fzf)
+    case $choose in
+        "Start iperf3 test")
+            choose_region
+            ;;
+        "Fetch newest iperf lists")
             fetch_iperf
-            exit 0
-            ;; 
-		*)
-            check_requirements && execute
-            exit 0
-			;;
-	esac
-done 
+            ;;
+        "Quit")
+            exit 1
+            ;;
+        *)
+            ;;
+    esac
+}
+
+check_requirements && menu
